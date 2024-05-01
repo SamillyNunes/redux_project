4a4ae6fc75faaf09dfb8e2adb45f6830ae949be6
Samilly <samillynunes19@gmail.com>
2024-05-01 14:27:37 -0300

    feat: logout action added

--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -10,7 +10,10 @@ export const userSlice = createSlice({
     reducers: {
         // definir quais serao as acoes que tera pra atualizar esse estado inicial do user
         createUser: (state, action) => {
-            console.log(action.payload);
+            if(action.payload.name.length<=4){
+                alert('Preencha o nome com pelo menos 4 caracteres!');
+                return { ...state };
+            }
             
             // Aqui no retorno da funcao, esta acontecendo duas coisas: primeiro mantem o estado que ja se tem
             // fazendo isso no "...state", e depois alterando so as propriedades que se quer no user: {...}
@@ -22,10 +25,16 @@ export const userSlice = createSlice({
                     address:null,
                 }
             }
+        },
+        logoutUser: (state) => {
+            return {
+                ...state,
+                user:null,
+            }
         }
     }
 });
 
-export const { createUser } = userSlice.actions;
+export const { createUser, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
