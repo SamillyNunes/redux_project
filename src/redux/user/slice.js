eae8239a9b57e6a12086670e61021c1210f02b5b
Samilly <samillynunes19@gmail.com>
2024-05-01 10:56:15 -0300

    feat; creating user with login

--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -9,7 +9,23 @@ export const userSlice = createSlice({
     initialState,
     reducers: {
         // definir quais serao as acoes que tera pra atualizar esse estado inicial do user
+        createUser: (state, action) => {
+            console.log(action.payload);
+            
+            // Aqui no retorno da funcao, esta acontecendo duas coisas: primeiro mantem o estado que ja se tem
+            // fazendo isso no "...state", e depois alterando so as propriedades que se quer no user: {...}
+            return {
+                ...state,
+                user: {
+                    name: action.payload.name,
+                    email: action.payload.email,
+                    address:null,
+                }
+            }
+        }
     }
 });
+
+export const { createUser } = userSlice.actions;
 
 export default userSlice.reducer;
